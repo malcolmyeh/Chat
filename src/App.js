b95165ef3889c3727bc61678deae10cea72ebbca
malcolmyeh <malcolmyeh@gmail.com>
2019-05-09 16:43:58 -0400

    reworked send message function

--- src/App.js
+++ src/App.js
@@ -1,105 +1,67 @@
-import React, { Component } from "react"
-import "./App.css"
-import firebase from "firebase"
-import { ListItem, ListItemText, TextField, List } from "@material-ui/core"
-
+import React, { Component } from "react";
+import "./App.css";
+import firebase from "firebase";
+import SendMessage from "./SendMessage";
+import { ListItem, ListItemText, TextField, List } from "@material-ui/core";
 
 class App extends Component {
- state = {
-   text: "",
-   name: "Anonymous",
-   messages: []
- }
-  componentDidMount(){
-   var config = {
-     apiKey: "AIzaSyBQ54OB7Rtu3OAh32yqQvnELLvGC3QiYT8",
-     authDomain: "anon-chat-23407.firebaseapp.com",
-     databaseURL: "https://anon-chat-23407.firebaseio.com",
-     projectId: "anon-chat-23407",
-     storageBucket: "anon-chat-23407.appspot.com",
-     messagingSenderId: "531896225805"
-   };
-   firebase.initializeApp(config);
-   this.getMessages()
- }
 
- onSubmit = event => {
-   if (event.charCode === 13 && this.state.text.trim() !== ""){ //if enter
-     console.log(this.state.text)
-     this.setState({ text: ""})
-     this.pushDB(this.state.text, this.state.name)
+  state = {
+    messages: []
+  };
+  componentDidMount() {
+    var config = {
+      apiKey: "AIzaSyBQ54OB7Rtu3OAh32yqQvnELLvGC3QiYT8",
+      authDomain: "anon-chat-23407.firebaseapp.com",
+      databaseURL: "https://anon-chat-23407.firebaseio.com",
+      projectId: "anon-chat-23407",
+      storageBucket: "anon-chat-23407.appspot.com",
+      messagingSenderId: "531896225805"
+    };
+    firebase.initializeApp(config);
+    this.getMessages();
+  }
 
-   }
- }
-  pushDB = (message, username) => {
-   firebase.database().ref("messages/").push({
-     text: message,
-     name: username,
-     time: Date(Date.now())
-   })
- }
+  getMessages = () => {
+    firebase
+      .database()
+      .ref("messages/")
+      .limitToLast(15)
+      .on("value", snapshot => {
+        let newMessages = [];
+        snapshot.forEach(child => {
+          var message = child.val();
+          newMessages.push({
+            id: child.key,
+            text: message.text,
+            name: message.name,
+            time: message.time
+          });
+        });
+        this.setState({ messages: newMessages });
+      });
+  };
 
- getMessages = () => {
-   firebase.database().ref("messages/").limitToLast(15).on("value", snapshot =>{
-     let newMessages = []
-     snapshot.forEach(child => {
-       var message = child.val()
-       newMessages.push({ id: child.key, text: message.text, name: message.name, time: message.time})
-     })
-     this.setState({messages: newMessages})
-   })
- }
+  renderMessages = () => {
+    return this.state.messages.map(message => (
+      <ListItem>
+        <ListItemText>{message.name + ": " + message.text}</ListItemText>
+        <ListItemText>         
+            {Math.floor((Date.now()-message.time)/60000) + " minutes ago"}
+        </ListItemText>
+      </ListItem>
+    ));
+  };
 
- renderMessages = () => {
-   return this.state.messages.map(message => (
-     <ListItem>
-       <ListItemText>
-         {message.name + ": " + message.text}    
-       </ListItemText>
-       <ListItemText>
-       {"sent at " + message.time.toString().split(' ')[4]}
-       </ListItemText>
-     </ListItem>
-   ))
- }
- handleNameChange = (e) => {
-   this.setState({
-       content: e.target.value
-   })
- }
- handleSubmit = (e) => {
-     e.preventDefault();
-     this.props.addTask(this.state);
-     this.setState({
-         content: ''
-     })
- }
- render() {
-   return (
-     <div className="App">
-       <h1>AnonChat</h1>
-       <TextField
-         required
-         label="Name"
-         onChange={event => this.setState({ name: event.target.value })}
-         value={this.state.name}
-         style={{ width: "98vw", overflow: "hidden" }}
-       />
-       <List>
-         {this.renderMessages()}
-       </List>
-       <TextField
-         placeholder="Press ENTER to submit. "
-         label="Enter text here"       
-         multiline={true}
-         onChange={event => this.setState({ text: event.target.value })}
-         value={this.state.text}
-         onKeyPress={this.onSubmit}
-         style={{ width: "98vw", overflow: "hidden" }}
-       /> 
-     </div>
-   )
- }
+  render() {
+    return (
+      <div className="App">
+        <h1>AnonChat</h1>
+        <SendMessage sendmessage />
+        <List>{this.renderMessages()}</List>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/SendMessage.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import firebase from "firebase";
+
+class SendMessage extends Component {
+  state = {
+    text: "",
+    name: "Anonymous"
+  };
+  handleNameChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+  handleTextChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+  handleTextSubmit = e => {
+    e.preventDefault();
+    this.pushDB(this.state.text, this.state.name);
+    this.setState({
+      text: ""
+    });
+  };
+  pushDB = (message, username) => {
+    firebase
+      .database()
+      .ref("messages/")
+      .push({
+        text: message,
+        name: username,
+        time: Date.now().toString()
+      });
+  };
+  render() {
+    return (
+      <div>
+        <form>
+          <label>Name: </label>
+          <input
+            type="text"
+            onChange={this.handleNameChange}
+            value={this.state.name}
+          />
+        </form>
+        <form onSubmit={this.handleTextSubmit}>
+          <label>Enter message: </label>
+          <input
+            type="text"
+            onChange={this.handleTextChange}
+            value={this.state.text}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+export default SendMessage;
