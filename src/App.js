eed0cdabc67c4973c148e5e4e7b666d663aacfc3
malcolmyeh <malcolmyeh@gmail.com>
2019-05-09 22:28:47 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import SendMessage from "./SendMessage";
 
 class App extends Component {
   state = {
-    messages: []
+    messages: [],
+    date: new Date()
   };
+
   componentDidMount() {
     var config = {
       apiKey: "AIzaSyBQ54OB7Rtu3OAh32yqQvnELLvGC3QiYT8",
@@ -18,17 +20,21 @@ class App extends Component {
     };
     firebase.initializeApp(config);
     this.getMessages();
-    this.interval = setInterval(() => this.renderMessages(), 100);
+    this.interval = setInterval(() => this.updateClock(), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-
+  updateClock() {
+    this.setState({
+      date: new Date()
+    });
+  }
   getMessages = () => {
     firebase
       .database()
       .ref("messages/")
-      .limitToLast(9)
+      .limitToLast(100)
       .on("value", snapshot => {
         let newMessages = [];
         snapshot.forEach(child => {
@@ -41,17 +47,35 @@ class App extends Component {
           });
         });
         this.setState({ messages: newMessages });
+        this.bottomSpan.scrollIntoView({ behavior: "smooth" })
       });
+      
   };
+
+  getTime = (currenttime, messagetime) => {
+    if ((Math.floor((currenttime - messagetime) / 60000)) < 1){
+      return " moments ago"
+    } else if ((Math.floor((currenttime - messagetime) / 60000)) == 1){
+      return 1 + " minute ago"
+    } else if ((Math.floor((currenttime - messagetime) / 60000)) < 60){
+      return (Math.floor((currenttime - messagetime) / 60000)) + " minutes ago"
+    } else if ((Math.floor((currenttime - messagetime) / 3600000)) == 1){
+    return 1 + "hour ago"
+    } else if ((Math.floor((currenttime - messagetime) / 3600000)) < 25){
+      return (Math.floor((currenttime - messagetime) / 3600000)) + " hours ago"
+    }else if ((Math.floor((currenttime - messagetime) / 86400000)) == 1){
+      return 1 + "hours ago"
+    }else {
+      return (Math.floor((currenttime - messagetime) / 86400000)) + " days ago"
+    } 
+  }
 
   renderMessages = () => {
     return this.state.messages.map(message => (
       <div>
         <p align="left">{message.name + ": " + message.text}</p>
         <p align="right">
-          {"sent " +
-            Math.floor((Date.now() - message.time) / 1) + //divide 60000
-            " minutes ago"}
+          {"sent " + this.getTime (this.state.date, message.time)}
         </p>
       </div>
     ));
@@ -60,9 +84,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Anonymous Chatroom</h1>
-        {this.renderMessages()}
-        <SendMessage sendmessage />
+        <nav>
+          <div class="navbar">
+            <a href="#" class="brand-logo">Anonymous Chatroom</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+              <li><a href="https://github.com/malcolmyeh/Chat">Source Code</a></li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          {/*<h2>Anonymous Chatroom</h2>*/}
+          {this.renderMessages()}
+          <SendMessage sendmessage />
+        </div>
+        <span ref={el => (this.bottomSpan = el)} />
       </div>
     );
   }
